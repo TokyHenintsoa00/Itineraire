8e159145889b76936b48563d16c44a2ca56e37ce
TokyHenintsoa00 <tokyramanalinarivo@gmail.com>
2026-09-10 17:07:57 +0300

    delete projet

--- app/src/main/java/com/example/itineraire/model/Arret.kt
+++
@@ -1,8 +0,0 @@
-package com.example.itineraire.model
-
-data class Arret(
-    val id: Int,
-    val nom: String,
-    val latitude: Double,
-    val longitude: Double
-)
--- app/src/main/java/com/example/itineraire/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.itineraire.model
-
-data class User(
-    val id: Int,
-    val nom: String,
-    val email: String,
-    val role: String
-)
